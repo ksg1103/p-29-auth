29efe06c64810cfa81ebff91e1d7a495fc89ab8c
ksg1103 <ksgwkd1103@naver.com>
2026-03-27 10:25:07 +0900

    기존 글,댓글 에서도 멤버,유저 포함

--- src/main/java/com/back/domain/member/service/MemberService.java
+++ src/main/java/com/back/domain/member/service/MemberService.java
@@ -5,6 +5,8 @@ import com.back.domain.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
 public class MemberService {
@@ -18,5 +20,14 @@ public class MemberService {
 
     public long count(){
         return memberRepository.count();
+    }
+
+    public Optional<Member> findByUsername(String username){
+//        return memberRepository.findAll().stream()
+//                .filter(m-> m.getUsername().equals(username))
+//                .findFirst(); //이방법도 있긴 한데, 대용량의 정보가 넘어오면 과부하 걸림
+//                              이경우는 어플리케이션에서 가공하는법
+        return memberRepository.findByUsername(username);
+        // 이경우는 데이터베이스에서 가공해서 가져오는 법
     }
 }
