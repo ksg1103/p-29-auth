0a1908beb8e2b9bf38f4512c67ed5490f8cf738e
ksg1103 <ksgwkd1103@naver.com>
2026-03-31 10:52:05 +0900

    헤더와 쿠키 방식 둘다 Rq에

--- src/main/java/com/back/global/rq/Rq.java
+++ src/main/java/com/back/global/rq/Rq.java
@@ -9,6 +9,8 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
+import java.util.Arrays;
+
 @Component
 //@RequestScope // 각 HTTP 요청마다 새로운 Rq 객체가 생성되고, 요청이 끝나면 해당 객체는 소멸됩니다.
 @RequiredArgsConstructor
@@ -29,14 +31,28 @@ public class Rq {
     public Member getActor() {
 
         String authorizationHeader = request.getHeader("Authorization");
+        String apiKey = null;
+        //헤더방식 vs 쿠키방식
+        if(authorizationHeader != null) {
+//            throw new ServiceException("401-2","인증 정보가 헤더에 존재하지 않습니다.");
 
-        if(authorizationHeader == null) {
-            throw new ServiceException("401-2","인증 정보가 헤더에 존재하지 않습니다.");
+            if(!authorizationHeader.startsWith("Bearer ")) {
+                throw new ServiceException("401-3","유효하지 않은 인증 정보입니다.");
+            }
+
+            apiKey = authorizationHeader.replace("Bearer ", ""); // "Bearer " 접두어 제거
+        } else{
+            apiKey = request.getCookies() == null ? ""
+                    : Arrays.stream(request.getCookies())
+                    .filter(cookie -> cookie.getName().equals("apiKey"))
+                    .map(Cookie::getValue)
+                    .findFirst()
+                    .orElseThrow(()-> new ServiceException("401-4","인증 정보가 쿠키에 존재하지 않습니다."));
         }
-        if(!authorizationHeader.startsWith("Bearer ")) {
-            throw new ServiceException("401-3","유효하지 않은 인증 정보입니다.");
+
+        if(apiKey.isBlank()){
+            throw new ServiceException("401-5","인증 정보가 존재하지 않습니다.");
         }
-        String apiKey = authorizationHeader.replace("Bearer ", ""); // "Bearer " 접두어 제거
 
         Member actor = memberService.findByApiKey(apiKey).orElseThrow(
                 () -> new ServiceException("401-1","유효하지 않은 API Key 입니다.")
