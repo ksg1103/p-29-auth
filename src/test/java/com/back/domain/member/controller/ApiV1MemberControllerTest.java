620d0e1904b26d1b8acd68ccbd8ca92f0aaedba6
ksg1103 <ksgwkd1103@naver.com>
2026-03-31 14:22:11 +0900

    jwt위한 의존성 추가, 테스트 환경 마련

--- src/test/java/com/back/domain/member/controller/ApiV1MemberControllerTest.java
+++ src/test/java/com/back/domain/member/controller/ApiV1MemberControllerTest.java
@@ -25,7 +25,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
-@ActiveProfiles("Membe-test")
+@ActiveProfiles("Member-test")
 @AutoConfigureMockMvc
 @Transactional
 public class ApiV1MemberControllerTest {
